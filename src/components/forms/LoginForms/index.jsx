ff11599b48c994243ab735cee7b06866bd14f79e
gabizinha <gabrielamiyazaki55@gmail.com>
2023-08-11 14:36:07 -0300

    update-request

--- src/components/forms/LoginForms/index.jsx
+++ src/components/forms/LoginForms/index.jsx
@@ -2,10 +2,15 @@ import { useForm } from "react-hook-form"
 import { Input } from "../Input"
 import { Link, useNavigate } from "react-router-dom"
 import logo from "../../../images/Logo.jpg"
+import { InputPassword } from "../Input/inputPassword"
+import { zodResolver} from "@hookform/resolvers/zod"
+import { FormSchema } from "./formSchema"
 
 export const LoginForm = () => {
     const Navigation = useNavigate()
-    const {register, handleSubmit} = useForm()
+    const {register, handleSubmit,  formState: {errors}} = useForm({
+        resolver: zodResolver(FormSchema)
+    })
 
     const Submit = (formData) => {
         Navigation("/register")
@@ -16,7 +21,9 @@ export const LoginForm = () => {
         <form onSubmit={handleSubmit(Submit)}>
             <img src={logo} alt="Logo Kenzie Hub" />
             <Input label="Email" type="email" placeholder="E-mail" {...register("email")} />
-            <Input label="Senha" type="password" placeholder="Senha" {...register("password")} />
+            {errors.email?.message}
+            <InputPassword label="Senha" placeholder="Senha" {...register("password")} />
+            {errors.password?.message}
             <button>Entrar</button>
             <p>Ainda não possui uma conta? </p>
             <Link to="/register" >Cadastre-se</Link>
--- src/components/forms/RegisterForm/index.jsx
+++ src/components/forms/RegisterForm/index.jsx
@@ -1,8 +1,13 @@
-import { useForm } from "react-hook-form"
-import { Input } from "../Input"
-import { useNavigate } from "react-router-dom"
+import { useForm } from "react-hook-form";
+import { Input } from "../Input";
+import { useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormSchemaRegister } from "./formsSchemaRegister";
+
 export const RegisterPage = () => {
-    const {register, handleSubmit} = useForm()
+    const {register, handleSubmit, formState: {errors}} = useForm({
+        resolver: zodResolver(FormSchemaRegister)
+    })
     const Navigation = useNavigate()
     
     const Submit = (formData) => {
@@ -12,11 +17,17 @@ export const RegisterPage = () => {
     return(
             <form onSubmit={handleSubmit(Submit)}>
                 <Input label="Nome" type="text" placeholder="Digite aqui seu nome" {...register("name")} />
+                {errors.name?.message}
                 <Input label="Email" type="email" placeholder="Digite aqui seu email" {...register("email")} />
+                {errors.email?.message}
                 <Input label="Senha" type="password" placeholder="Digite aqui sua senha" {...register("password")} />
+                {errors.password?.message}
                 <Input label="Confirmar Senha" type="password" placeholder="Digite novamente sua senha" {...register("password")} />
-                <Input label="Bio" type="text" placeholder="Fale sobre você" {...register("password")} />
+                {errors.password?.message}
+                <Input label="Bio" type="text" placeholder="Fale sobre você" {...register("description")} />
+                {errors.description?.message}
                 <Input label="Contato" type="text" placeholder="Opção de contato" {...register("contact")} />
+                {errors.contacts?.message}
                 <select>
                     <option>Primeiro modulo</option>
                     <option>Segundo modulo</option>
